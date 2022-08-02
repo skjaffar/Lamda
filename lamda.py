cce11b9fe3f945479d589f7e90b15d780f6a304d
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-08-02 01:40:32 -0700

    Add files via upload

---
+++ lamda.py
@@ -0,0 +1,40 @@
+# f  = lambda x: x**2
+# value = f(8)
+# print('square of 5 =', value)
+
+# ''' pre-Defined Function '''
+# ''' sorted '''
+# a=[1,6,5,8,2]
+# a.sort()
+# print(a)
+# print(sorted([12,3,150,60]))
+# print(sorted(12,3,4,80))
+
+# ''' all '''
+# print(all([True,1,2]))
+# print(all([True,'',1,2]))
+# print(all([True,' ',1,2]))
+# print(all([True,False,1,2]))
+# print(all([True,True,1,2,0]))
+# print(all([True,True,1,2,None]))
+
+# ''' any '''
+# print(any([True,False,False,23]))
+# print(any([False,False,0]))
+# print(any([True,True,None,0]))
+
+# ''' eval() '''
+# print('eval=',eval('5+6,8-1'))
+# a=eval('5+6-4')
+# b=eval('4+8,8-9')
+# print(a,type(a))
+# print(b,type(b))
+
+# ''' bool() '''
+
+# print(bool(False))
+# print(bool(1))
+# print(bool(3))
+# print(bool(None))
+# print(bool('bool'))
+
